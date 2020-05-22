678d23a26e05ee12a351e21a37a0dc3d50922ec4
Charlie-jinn-DQ <65146680+Charlie-jinn-DQ@users.noreply.github.com>
2020-05-22 11:39:45 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,14 +18,14 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-`┈╭━━━━━━━━━━━╮┈
- ┈┃╭━━━╮┊╭━━━╮┃┈
- ╭┫┃┈▇┈┃┊┃┈▇┈┃┣╮
- ┃┃╰━━━╯┊╰━━━╯┃┃
- ╰┫╭━╮╰━━━╯╭━╮┣╯
- ┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈
- ┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈
- ┈╰━━━━━━━━━━━╯┈
+┈╭━━━━━━━━━━━╮┈
+┈┃╭━━━╮┊╭━━━╮┃┈
+╭┫┃┈▇┈┃┊┃┈▇┈┃┣╮
+┃┃╰━━━╯┊╰━━━╯┃┃
+╰┫╭━╮╰━━━╯╭━╮┣╯
+┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈
+┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈
+┈╰━━━━━━━━━━━╯┈
 ╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗
 ║║║╠─║─║─║║║║║╠─
 ╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😬ADD YOUR GROUP😬", url="t.me/{}?startgroup=true".format(bot.username))]]))
+                parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🛅ADD ME TO YOUR GROUP🛅", url="t.me/{}?startgroup=true".format(bot.username))]]))
     else:
         update.effective_message.reply_text("")
 
